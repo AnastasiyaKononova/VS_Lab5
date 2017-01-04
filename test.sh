476703cb92f15652c1640038537d9de68a8c9f38
Anastasiya <anastkononova@gmail.com>
2017-01-05 01:40:08 +0300

    add load and delete unload

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ function check {
     echo $1 > /dev/first
     echo $2 > /dev/operator
     echo $3 > /dev/second
-	result=$(cat /proc/result)
+	 result=$(cat /proc/result)
     if [ "$4" == "$result" ]; then
         echo "Yes"
     else
@@ -27,5 +27,4 @@ check 2 p 4 "8"
 check 3 p 2 "6"
 check 9 p 2 "10"
 
-sudo sh unload.sh
 
